9326c42cde5ba1f12df44ae42bbcd6672361d416
waldo7 <waldo.ramones@gmail.com>
2025-05-03 17:58:21 +0800

    feat: Register Client model with Django admin

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Client
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    """Configuration for the Client model in the Django admin interface"""
+    list_display = ("name", "email", "phone", "created_at")
+    search_fields = ("name", "email")
+    list_filter = ("created_at",)
